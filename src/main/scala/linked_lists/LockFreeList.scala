921013d38b9cc8a7a275f49a9c4b52f48160fa59
Oleh Bodunov <oleg.bdnf@gmail.com>
2019-01-27 20:54:08 +0100

    changed implementation

--- src/main/scala/linked_lists/LockFreeList.scala
+++ src/main/scala/linked_lists/LockFreeList.scala
@@ -19,7 +19,6 @@ class LockFreeList {
   while (!this.head.next.compareAndSet(null, this.tail, false, false)){}
 
 
-
   def find(head: NodeMark[Int], key: Int): (NodeMark[Int], NodeMark[Int]) = {
     var pred: NodeMark[Int] = null
     var curr: NodeMark[Int] = null
@@ -28,7 +27,7 @@ class LockFreeList {
     // is curr marked?
     var snip = false
 
-    def cas(): Option[(NodeMark[Int], NodeMark[Int])] = {
+    def findPair: (NodeMark[Int], NodeMark[Int]) = {
       while (true) {
         pred = head
         curr = pred.next.getReference
@@ -37,45 +36,56 @@ class LockFreeList {
           while (marked(0)) {
             // replace curr if marked
             snip = pred.next.compareAndSet(curr, succ, false, false)
-            if (!snip) cas() //sentinel node wiill never be marked
+            if (!snip) findPair //sentinel node wiill never be marked
             curr = pred.next.getReference
             succ = curr.next.get(marked)
-
-            if (curr.key >= key) return Some((pred, curr))
-            pred = curr
-            curr = succ
           }
+          if (curr.key >= key) return (pred, curr)
+          pred = curr
+          curr = succ
         }
       }
-      None
+      (pred, curr)
     }
 
-    cas() match {
-      case Some(window) => return window
-      case None => null
-    }
+    findPair
+
 
   }
 
   def add(item: Int): Boolean = {
+    println("Adding node")
     val key = item.hashCode
+    if (head.next.getReference != null) {
+      while (true) { // find predecessor and current entries
 
-    while (true) { // find predecessor and current entries
-      val window = find(head, key)
-      val pred = window._1
-      val curr = window._2
+        val window = find(head, key)
+        val pred = window._1
+        val curr = window._2
 
-      if (curr.key == key) return false
-      else { // splice in new node
-        val node = new NodeMark(item, key, new AtomicMarkableReference(curr, false))
+        if (curr.key == key) return false
+        else { // splice in new node
 
-        if (pred.next.compareAndSet(curr, node, false, false)) return true
+          val node = new NodeMark(item, key, new AtomicMarkableReference(curr, false))
+
+          if (pred.next.compareAndSet(curr, node, false, false)) {
+            println("Node added: ", item)
+            return true
+          }
+        }
+      }
+    } else {
+      val node = new NodeMark(item, key, new AtomicMarkableReference(head, false))
+
+      if (head.next.compareAndSet(null, node, false, false)) {
+        return true
       }
     }
     return false
   }
 
   def remove(item: Int): Boolean = {
+    println("Removing node", item)
     val key = item.hashCode
     var snip = false
     while (true) { // find predecessor and curren entries
@@ -105,9 +115,38 @@ class LockFreeList {
     curr.key == key
   }
 
+//  def foreach(f: Int => Boolean): Unit = {
+//    val curr = head
+//    while(curr.next == null)
+//    f(curr.next)
+//  }
 
+  def printElements: Unit = {
+   val curr = head
+   var next = curr.next.getReference
+   while(next.key < tail.key){
+     print("Element: ", next.item)
+     next = next.next.getReference
+   }
+  }
 
+}
 
+object LockFreeList extends App {
+
+  var list = new LockFreeList
+  val threads = (1 to 10).map(i => {
+    new Thread(() => {
+      if (i%5 == 0) list.remove(scala.util.Random.nextInt(10))
+      else list.add(scala.util.Random.nextInt(10))
+    })
+  })
+
+  //threads.foreach(t => println(t.getId.asInstanceOf[Int]))
+  threads.foreach(_.start())
+  threads.foreach(_.join())
+  print("List contains: ")
+  list.printElements
 }
 
 
